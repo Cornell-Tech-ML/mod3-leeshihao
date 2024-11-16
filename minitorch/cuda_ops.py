3bc4991b702f25db993d90cbe4b9c74cf2ed339f
leeshihao <leeshihao1@gmail.com>
2024-11-15 22:14:19 -0500

    handle batching

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -509,29 +509,28 @@ def _tensor_matrix_multiply(
     bk_stride = b_strides[-2]
     bj_stride = b_strides[-1]
     max_k = a_shape[-1]
-    # Handle batching
-    if batch < a_shape[0] and batch < b_shape[0]:
-        # Load data into shared memory
-        acc = 0.0
-        for k in range(0, max_k, BLOCK_DIM):
-            # Convert index to contiguous index
-            if i < a_shape[-2] and k + pi < max_k:
-                a_pos = batch * a_batch_stride + i * ai_stride + (k + pi) * ak_stride
-                a_shared[pi, pj] = a_storage[a_pos]
-            if j < b_shape[-1] and k + pj < max_k:
-                b_pos = batch * b_batch_stride + (k + pj) * bk_stride + j * bj_stride
-                b_shared[pi, pj] = b_storage[b_pos]
-            cuda.syncthreads()
 
-            # Compute output
-            for local_k in range(min(BLOCK_DIM, max_k - k)):
-                acc += a_shared[pi, local_k] * b_shared[local_k, pj]
-            cuda.syncthreads()
+    # Load data into shared memory
+    acc = 0.0
+    for k in range(0, max_k, BLOCK_DIM):
+        # Convert index to contiguous index
+        if i < a_shape[-2] and k + pi < max_k:
+            a_pos = batch * a_batch_stride + i * ai_stride + (k + pi) * ak_stride
+            a_shared[pi, pj] = a_storage[a_pos]
+        if j < b_shape[-1] and k + pj < max_k:
+            b_pos = batch * b_batch_stride + (k + pj) * bk_stride + j * bj_stride
+            b_shared[pi, pj] = b_storage[b_pos]
+        cuda.syncthreads()
 
-        # Calculate index of out
-        if i < a_shape[-2] and j < b_shape[-1]:
-            out_pos = batch * out_strides[0] + i * out_strides[-2] + j * out_strides[-1]
-            out[out_pos] = acc
+        # Compute output
+        for local_k in range(min(BLOCK_DIM, max_k - k)):
+            acc += a_shared[pi, local_k] * b_shared[local_k, pj]
+        cuda.syncthreads()
+
+    # Calculate index of out
+    if i < a_shape[-2] and j < b_shape[-1]:
+        out_pos = batch * out_strides[0] + i * out_strides[-2] + j * out_strides[-1]
+        out[out_pos] = acc
 
 
 tensor_matrix_multiply = jit(_tensor_matrix_multiply)
