7547829c8b85b88b027c02de520aa8de4e52beac
leeshihao <leeshihao1@gmail.com>
2024-11-20 21:07:19 -0500

    simplified mm_practice and fixed mm

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -423,24 +423,24 @@ def _mm_practice(out: Storage, a: Storage, b: Storage, size: int) -> None:
         (BLOCK_DIM, BLOCK_DIM), numba.float64
     )  # Shared memory for array b
 
-    i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x  # Global i position
-    j = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y  # Global j position
     local_i = cuda.threadIdx.x  # Local i position
     local_j = cuda.threadIdx.y  # Local j position
 
     # Load data into shared memory
-    if i < size and j < size:  # Make sure we don't go out of bounds for output
+    if (
+        local_i < size and local_j < size
+    ):  # Make sure we don't go out of bounds, local = global since size < 32
         acc = 0.0  # Accumulator
         for k in range(
             0, size, BLOCK_DIM
         ):  # Loop over size with stride of BLOCK_DIM to tackle a block at a time
             if k + local_j < size:  # Make sure we don't go out of bounds for a
                 a_shared[local_i, local_j] = a[
-                    i * size + (k + local_j)
+                    local_i * size + (k + local_j)
                 ]  # Load data into shared memory for a
             if k + local_i < size:  # Make sure we don't go out of bounds for b
                 b_shared[local_i, local_j] = b[
-                    (k + local_i) * size + j
+                    (k + local_i) * size + local_j
                 ]  # Load data into shared memory for b
             cuda.syncthreads()  # Synchronize threads
 
@@ -450,7 +450,7 @@ def _mm_practice(out: Storage, a: Storage, b: Storage, size: int) -> None:
                 acc += (
                     a_shared[local_i, local_k] * b_shared[local_k, local_j]
                 )  # Accumulate the result
-        out[i * size + j] = acc  # Write the result to global memory
+        out[local_i * size + local_j] = acc  # Write the result to global memory
 
 
 jit_mm_practice = jit(_mm_practice)
@@ -532,9 +532,7 @@ def _tensor_matrix_multiply(
     bj_stride = b_strides[-1]  # Stride for bj
     max_k = a_shape[-1]  # Max k
 
-    if (
-        batch < out_shape[0] and i < out_shape[-2] and j < out_shape[-1]
-    ):  # Check that batch, i and j are in bounds
+    if batch < out_shape[0]:  # Check that batch is in bounds
         # Load data into shared memory
         acc = 0.0  # Accumulator
         # 1) Move across shared dimension by block dim.
@@ -542,13 +540,17 @@ def _tensor_matrix_multiply(
             0, max_k, BLOCK_DIM
         ):  # Loop over size with stride of BLOCK_DIM to tackle a block at a time
             # a) Copy into shared memory for a matrix.
-            if k + pj < max_k:  # Check that k + pj is in bounds for a
+            if (
+                i < a_shape[-2] and k + pj < max_k
+            ):  # Check that i and k + pj is in bounds for a
                 a_pos = (
                     batch * a_batch_stride + i * ai_stride + (k + pj) * ak_stride
                 )  # Convert index (batch, i, k + pj) to contiguous index
                 a_shared[pi, pj] = a_storage[a_pos]  # Copy into shared
             # b) Copy into shared memory for b matrix.
-            if k + pi < max_k:  # Check that k + pi are in bounds for b
+            if (
+                j < b_shape[-1] and k + pi < max_k
+            ):  # Check that j and k + pi are in bounds for b
                 b_pos = (
                     batch * b_batch_stride + (k + pi) * bk_stride + j * bj_stride
                 )  # Convert index (batch, k + pi, j) to contiguous index
@@ -564,10 +566,11 @@ def _tensor_matrix_multiply(
                 )  # Accumulate the result
 
         # Calculate index of out
-        out_pos = (
-            batch * out_strides[0] + i * out_strides[-2] + j * out_strides[-1]
-        )  # Convert index (batch, i, j) to contiguous index
-        out[out_pos] = acc  # Store result
+        if i < out_shape[-2] and j < out_shape[-1]:  # Check that i and j are in bounds
+            out_pos = (
+                batch * out_strides[0] + i * out_strides[-2] + j * out_strides[-1]
+            )  # Convert index (batch, i, j) to contiguous index
+            out[out_pos] = acc  # Store result
 
 
 tensor_matrix_multiply = jit(_tensor_matrix_multiply)
